5946667ba920afed442a5a5fcc3dab16a4c29971
Eliana Ritzmann <eliana.cristinas2@hotmail.com>
2025-08-23 21:28:35 -0300

    instalação router

--- meu-app-react/src/App.jsx
+++ meu-app-react/src/App.jsx
@@ -1,12 +1,17 @@
 import { useState } from 'react'
 import './App.css'
-
+import { Route, Routes } from "react-router";
+import Cards from './Cards'; 
+import Tabelas from './Tabelas';
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      
+      <Routes>
+         <Route path="/" Component={Cards} />
+         <Route path="/criar-conta" Component={Tabelas} />
+      </Routes>
         
         
     </>
